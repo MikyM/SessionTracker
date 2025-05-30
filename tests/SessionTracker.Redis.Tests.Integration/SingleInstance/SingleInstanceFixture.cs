b24d7653ca9df7f7c6ca3c062a0da024a5fea05a
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2025-05-30 16:01:45 +0200

    Weird net9 redis integration test behavior

--- tests/SessionTracker.Redis.Tests.Integration/SingleInstance/SingleInstanceFixture.cs
+++ tests/SessionTracker.Redis.Tests.Integration/SingleInstance/SingleInstanceFixture.cs
@@ -78,6 +78,8 @@ public class SingleInstanceRedisFixture : RedisFixture
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
+
+                throw;
             }
 
 
